0155c61c198b5cc96d021594f3e200970712a456
vidyalak <bvidya252@gmail.com>
2025-09-28 10:57:13 +0530

    Rendering a List with ternary operator

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ProductList from "./components/ProductList";
 import IPhoneList from "./components/IPhoneList";
+import LaptopList from "./components/LaptopList";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <h1>Hello React!</h1>
       <ProductList />
       <IPhoneList/>
+      <LaptopList/>
     </div>
   );
 }
